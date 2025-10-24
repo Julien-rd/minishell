05167b9bee357e09153ec590e6f7d662114f89fb
Eric Prottung <ericprottung@gmail.com>
2025-10-24 17:01:04 +0200

    added print declare -x

--- exec/builtins/builtin_handler.c
+++ exec/builtins/builtin_handler.c
@@ -12,11 +12,34 @@
 
 #include "minishell.h"
 
+static void  print_declare_x(t_pipeline *pl, t_sh *sh)
+{
+   size_t  iter;
+
+
+   iter = 0;
+   if (pl->current->argv[1])
+	return ;
+   while (sh->envp.vars[iter])
+   {
+       if (safe_write(1, "declare -x " , 12))
+           child_exit(sh, pl, 1);
+       if (safe_write(1, sh->envp.vars[iter], ft_strlen(sh->envp.vars[iter])) == -1)
+           child_exit(sh, pl, 1);
+       if (safe_write(1, "\n" , 1))
+           child_exit(sh, pl, 1);
+       iter++;
+   }
+   child_exit(sh, pl, 0);
+}
+
 void	builtin_handler(t_pipeline *pl, t_sh *sh)
 {
 	int	flag;
 
 	flag = options_check(pl->current);
+	if (pl->current->cmd_flag == EXPORT)
+		print_declare_x(pl, sh);
 	if (pl->current->cmd_flag == ECHO)
 		echo(pl, sh, flag);
 	else if (pl->current->cmd_flag == PWD)
